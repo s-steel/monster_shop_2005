41fd9f7db08cd863367148b563c1ea7f86fbcf6e
Zach Stearns <zach.stearns92@gmail.com>
2020-10-27 14:14:46 -0600

    Add 404 capability for visitor visiting merchant dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user, :current_admin?
+  helper_method :cart, :current_user, :current_admin?, :current_merchant?
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
@@ -13,5 +13,9 @@ class ApplicationController < ActionController::Base
 
   def current_admin?
     current_user && current_user.admin?
+  end
+
+  def current_merchant?
+    current_user && current_user.merchant?
   end
 end
