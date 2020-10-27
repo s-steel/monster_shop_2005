bdba66e354d2a9b850fe63b420100ed00ded9c1c
Zach Stearns <zach.stearns92@gmail.com>
2020-10-27 13:38:48 -0600

    Add functionality for 404 for visitor on admin users index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,11 @@
 class Admin::UsersController < ApplicationController
+  before_action :require_admin
+
   def index
+  end
+
+private
+  def require_admin
+    render file: "/public/404" unless current_admin?
   end
 end
