57cff81a2185066dafbbc5d51c058d0fdb840946
Aidan Murray <aidanhansm@gmail.com>
2020-10-26 20:31:47 -0600

    Refactor user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,7 @@ class UsersController < ApplicationController
       flash[:success] = 'You are now registered and logged in'
       redirect_to '/profile'
     rescue ActiveRecord::RecordInvalid => e
-      case e.message
-      when "Validation failed: Name can't be blank, Address can't be blank, City can't be blank, State can't be blank, Zip can't be blank"
-        flash[:error] = 'Please enter data in all required fields'
-        redirect_to '/register'
-      when 'Validation failed: Email has already been taken'
-        flash[:error] = 'This email is already registered. Please use a new email.'
-        redirect_to '/register'
-      end
+      create_error_response(e)
     end
   end
 
@@ -34,5 +27,16 @@ class UsersController < ApplicationController
                                  :zip,
                                  :email,
                                  :password)
+  end
+
+  def create_error_response(error)
+    case error.message
+    when "Validation failed: Name can't be blank, Address can't be blank, City can't be blank, State can't be blank, Zip can't be blank"
+      flash[:error] = 'Please enter data in all required fields'
+      redirect_to '/register'
+    when 'Validation failed: Email has already been taken'
+      flash[:error] = 'This email is already registered. Please use a new email.'
+      redirect_to '/register'
+    end
   end
 end
