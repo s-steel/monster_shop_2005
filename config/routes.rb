ecff3a4ed754459c4399458bc6d8b1c8f54878e0
Zach Stearns <zach.stearns92@gmail.com>
2020-10-27 13:34:14 -0600

    Add namespace for admin

--- config/routes.rb
+++ config/routes.rb
@@ -42,4 +42,10 @@ Rails.application.routes.draw do
   get '/profile', to: 'users#show'
 
   get '/login', to: 'sessions#new'
+
+  # Admin
+  namespace :admin do
+    get '/', to: 'dashboard#index'
+    get '/users', to: 'users#index'
+  end
 end
