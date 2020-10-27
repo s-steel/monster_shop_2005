bb7db3d473106f2144b36117ea423528817a68e3
Zach Stearns <zach.stearns92@gmail.com>
2020-10-27 12:28:04 -0600

    Add merchant employee nav dashboard link test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,52 @@ describe User, type: :model do
       should validate_uniqueness_of :email
     end
   end
+
+  describe "roles" do
+    it "can be created as a default user" do
+      user = User.create!(name: 'Mike Dao',
+                          address: '123 Main St',
+                          city: 'Denver',
+                          state: 'CO',
+                          zip: '80428',
+                          email: 'mike@turing.com',
+                          password: 'ilikefood',
+                          role: 0)
+
+      expect(user.role).to eq("default")
+      expect(user.default?).to be_truthy
+    end
+
+    it "can be created as a merchant employee" do
+      user = User.create!({
+        name: "Kyle",
+        address: "333 Starlight Ave.",
+        city: "Bakersville",
+        state: "NV",
+        zip: '90210',
+        email: "kyle@email.com",
+        password: "word",
+        role: 1
+        })
+
+      expect(user.role).to eq("merchant_employee")
+      expect(user.merchant_employee?).to be_truthy
+    end
+
+    xit "can be created as an admin" do
+      user = User.create!({
+        name: "Bruce Wayne",
+        address: "456 Batcave Alley",
+        city: "Gotham",
+        state: "NY",
+        zip: '77568',
+        email: "batguy@email.com",
+        password: "twoface",
+        role: 2
+        })
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+  end
 end
