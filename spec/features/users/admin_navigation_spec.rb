30781e76e66e03aec34775057e9f9b3478bc9ce7
Sean Steel <seanrsteel@gmail.com>
2020-10-27 15:18:13 -0600

    Finish user story 9, admin navigarion restrictions

--- spec/features/users/admin_navigation_spec.rb
+++ spec/features/users/admin_navigation_spec.rb
@@ -36,4 +36,34 @@ describe "As an admin" do
       expect(page).to have_content("Logged in as #{admin.name}")
     end
   end
+
+  describe 'Merchant navigation restrictions' do
+
+    before :each do
+      admin = User.create!({
+        name: "Bruce Wayne",
+        address: "456 Batcave Alley",
+        city: "Gotham",
+        state: "NY",
+        zip: '77568',
+        email: "batguy@email.com",
+        password: "twoface",
+        role: 2
+        })
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    end
+
+    it 'merchant cannot see /merchant' do
+      visit "/merchant"
+
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+
+    it 'merchant cannot see /cart' do
+      visit "/cart"
+
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
 end
