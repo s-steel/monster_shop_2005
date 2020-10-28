4de85bab39cf41ebdfc6f9df26312df3f9e6ae52
Zach Stearns <zach.stearns92@gmail.com>
2020-10-28 12:39:02 -0600

    Write test for editing user profile

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -36,5 +36,27 @@ describe "As a registered user" do
 
       expect(page).to have_link("Edit Profile")
     end
+
+    it "when I click on the link to edit my profile, I see a form that's prepopulated
+        with all current information besides password.  After changing and submitting,
+        I am returned to my profile page and see a flash that info is updated" do
+      visit '/profile'
+
+      click_link "Edit Profile"
+      expect(current_path).to eq("/profile/edit")
+
+      expect(find_field(:name).value).to eq("#{@user.name}")
+      expect(find_field(:address).value).to eq("#{@user.address}")
+      expect(find_field(:city).value).to eq("#{@user.city}")
+      expect(find_field(:state).value).to eq("#{@user.state}")
+      expect(find_field(:zip).value).to eq("#{@user.zip}")
+      expect(find_field(:email).value).to eq("#{@user.email}")
+
+      fill_in :name, with "Harry Guy"
+      click_button('Submit')
+      
+      expect(current_path).to eq('/profile')
+      expect(page).to have_content("Harry Guy")
+    end
   end
 end
