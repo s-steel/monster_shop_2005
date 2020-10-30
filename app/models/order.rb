0ca9195a4515587dc8ba0792dc3eba096327ef7d
Sean Steel <seanrsteel@gmail.com>
2020-10-30 10:19:21 -0400

    Finish user story 28, profile orders are displayed with info

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,9 +5,21 @@ class Order <ApplicationRecord
   has_many :items, through: :item_orders
   belongs_to :user
 
-  enum role: %w(packaged pending shipped cancelled)
+  enum status: %w(packaged pending shipped cancelled)
 
   def grandtotal
     item_orders.sum('price * quantity')
+  end
+
+  def item_count
+    item_orders.sum(:quantity)
+  end
+
+  def date_created
+    created_at.strftime('%m/%d/%Y')
+  end
+
+  def date_updated
+    updated_at.strftime('%m/%d/%Y')
   end
 end
--- spec/features/orders/show_spec.rb
+++ spec/features/orders/show_spec.rb
@@ -19,31 +19,13 @@ describe 'Order show page' do
       @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
       @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
 
-      visit "/items/#{@paper.id}"
-      click_on "Add To Cart"
-      visit "/items/#{@paper.id}"
-      click_on "Add To Cart"
-      visit "/items/#{@tire.id}"
-      click_on "Add To Cart"
-      visit "/items/#{@pencil.id}"
-      click_on "Add To Cart"
+      @order_1 = Order.create!(name: 'order', address: '123 Main St', city: 'Here', state: 'CO', zip: '58421', user_id: @user.id)
+      item_order_1 = @order_1.item_orders.create!(item_id: @tire.id, price: @tire.price, quantity: 2)
+      item_order_2 = @order_1.item_orders.create!(item_id: @paper.id, price: @paper.price, quantity: 3)
 
-      visit "/cart"
-      click_on "Checkout"
-      name = "Bert"
-      address = "123 Sesame St."
-      city = "NYC"
-      state = "New York"
-      zip = 10001
-
-      fill_in :name, with: name
-      fill_in :address, with: address
-      fill_in :city, with: city
-      fill_in :state, with: state
-      fill_in :zip, with: zip
-      click_button "Create Order"
-
-      @order = Order.last
+      @order_2 = Order.create!(name: 'second order', address: '754 Main St', city: 'There', state: 'WY', zip: '12421', user_id: @user.id)
+      item_order_3 = @order_2.item_orders.create!(item_id: @paper.id, price: @paper.price, quantity: 1)
+      item_order_3 = @order_2.item_orders.create!(item_id: @pencil.id, price: @pencil.price, quantity: 2)
     end
 
     it 'user profile displays link' do
@@ -56,18 +38,30 @@ describe 'Order show page' do
     it 'visit /profile/orders and see every order the user has made' do
       visit '/profile/orders'
       expect(page).to have_content('Your Orders:')
-      expect(page).to have_content('Order ID:')
-      expect(page).to have_content('Date order was made:')
-      expect(page).to have_content('Date order was last updated:')
-      expect(page).to have_content('Order Status:')
-      expect(page).to have_content('Quantity of items in order:')
-      expect(page).to have_content('Grand total:')
-      expect(page).to have_content(@order.id)
-      expect(page).to have_content(@order.created_at)
-      expect(page).to have_content(@order.updated_at)
-      expect(page).to have_content(@order.status)
-      expect(page).to have_content(@order.items.count)
-      expect(page).to have_content(@order.grandtotal)
+
+      within "#order-#{@order_1.id}" do
+        expect(page).to have_content('Order ID:')
+        expect(page).to have_content('Date order was made:')
+        expect(page).to have_content('Date order was last updated:')
+        expect(page).to have_content('Order Status:')
+        expect(page).to have_content('Quantity of items in order:')
+        expect(page).to have_content('Grand total:')
+        expect(page).to have_content(@order_1.id)
+        expect(page).to have_content(@order_1.created_at.strftime('%m/%d/%Y'))
+        expect(page).to have_content(@order_1.updated_at.strftime('%m/%d/%Y'))
+        expect(page).to have_content(@order_1.status)
+        expect(page).to have_content(@order_1.items.count)
+        expect(page).to have_content("$#{@order_1.grandtotal}")
+      end
+
+      within "#order-#{@order_2.id}" do
+        expect(page).to have_content(@order_2.id)
+        expect(page).to have_content(@order_2.created_at.strftime('%m/%d/%Y'))
+        expect(page).to have_content(@order_2.updated_at.strftime('%m/%d/%Y'))
+        expect(page).to have_content(@order_2.status)
+        expect(page).to have_content(@order_2.items.count)
+        expect(page).to have_content("$#{@order_2.grandtotal}")
+      end
     end
   end
 end
