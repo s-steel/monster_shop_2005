1aa8157b3d47cb4fc7661d59a4c258cc69b5454a
Zach Stearns <zach.stearns92@gmail.com>
2020-11-01 16:35:16 -0700

    WIP test for merchant dashboard index

---
+++ spec/features/merchant/dashboard/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "As a merchant employee" do
+  describe "When I visit my merchant dashboard" do
+    before(:each) do
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 23137)
+      @merch_employee = User.create!({
+        name: "Kyle",
+        address: "333 Starlight Ave.",
+        city: "Bakersville",
+        state: "NV",
+        zip: '90210',
+        email: "kyle@email.com",
+        password: "word",
+        role: 1,
+        merchant_id: @bike_shop.id
+        })
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merch_employee)
+    end
+    it "I see the name and full address of the merchant I work for" do
+      visit "/merchant"
+      save_and_open_page
+    end
+  end
+end
