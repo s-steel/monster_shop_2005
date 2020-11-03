b6d8f0882585da773b88fe92348a4b21b78fa115
Aidan Murray <aidanhansm@gmail.com>
2020-11-03 13:09:56 -0700

    Add spec for merchant_item_orders method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -33,7 +33,7 @@ describe Merchant, type: :model do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
-    it 'no_orders' do
+    it '#no_orders' do
       expect(@meg.no_orders?).to eq(true)
 
       order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17_033)
@@ -42,19 +42,19 @@ describe Merchant, type: :model do
       expect(@meg.no_orders?).to eq(false)
     end
 
-    it 'item_count' do
+    it '#item_count' do
       chain = @meg.items.create(name: 'Chain', description: "It'll never break!", price: 30, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 22)
 
       expect(@meg.item_count).to eq(2)
     end
 
-    it 'average_item_price' do
+    it '#average_item_price' do
       chain = @meg.items.create(name: 'Chain', description: "It'll never break!", price: 40, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 22)
 
       expect(@meg.average_item_price).to eq(70)
     end
 
-    it 'distinct_cities' do
+    it '#distinct_cities' do
       chain = @meg.items.create(name: 'Chain', description: "It'll never break!", price: 40, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 22)
       order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17_033)
       order_2 = @user.orders.create!(name: 'Brian', address: '123 Brian Ave', city: 'Denver', state: 'CO', zip: 17_033)
@@ -67,7 +67,7 @@ describe Merchant, type: :model do
       expect(@meg.distinct_cities).to include('Hershey')
     end
 
-    it 'shows associated_orders' do
+    it '#associated_orders' do
       @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80_203)
       @paper = @mike.items.create(name: 'Lined Paper', description: 'Great for writing on!', price: 20, image: 'https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png', inventory: 360)
 
@@ -94,7 +94,7 @@ describe Merchant, type: :model do
       expect(@meg.associated_orders).to_not include(@order_2)
     end
 
-    it 'shows pending orders associated with this merchant' do
+    it '#pending_orders' do
       @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80_203)
       @paper = @mike.items.create(name: 'Lined Paper', description: 'Great for writing on!', price: 20, image: 'https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png', inventory: 360)
 
@@ -116,6 +116,32 @@ describe Merchant, type: :model do
       @item_order_9 = @order_3.item_orders.create!(item_id: @paper.id, price: @paper.price, quantity: 7)
 
       expect(@meg.pending_orders).to eq([@order_1])
+    end
+
+    it '#merchant_item_orders' do
+      @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80_203)
+      @paper = @mike.items.create(name: 'Lined Paper', description: 'Great for writing on!', price: 20, image: 'https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png', inventory: 360)
+
+      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 55)
+      @pedal = @meg.items.create(name: "Pedal", description: "Step on it", price: 10, image: "https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/11/2020/06/meow.jpg", inventory: 374)
+      @reflector = @meg.items.create(name: "Reflector", description: "They'll see you", price: 200, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcStDVPL-iMrlZHu5mai-EXeyDH-wH2r9nBeFw&usqp=CAU", inventory: 324)
+
+      @order_1 = Order.create!(name: 'order', address: '123 Main St', city: 'Here', state: 'CO', zip: '58421', user_id: @user.id, status: 'pending')
+      @item_order_1 = @order_1.item_orders.create!(item_id: @tire.id, price: @tire.price, quantity: 2)
+      @item_order_2 = @order_1.item_orders.create!(item_id: @paper.id, price: @paper.price, quantity: 3)
+      @item_order_3 = @order_1.item_orders.create!(item_id: @chain.id, price: @chain.price, quantity: 1)
+
+      @order_2 = Order.create!(name: 'second order', address: '754 Main St', city: 'There', state: 'WY', zip: '12421', user_id: @user.id, status: 'pending')
+      @item_order_6 = @order_2.item_orders.create!(item_id: @paper.id, price: @paper.price, quantity: 7)
+
+      @order_3 = Order.create!(name: 'second order', address: '754 Main St', city: 'There', state: 'WY', zip: '12421', user_id: @user.id, status: 'shipped')
+      @item_order_7 = @order_3.item_orders.create!(item_id: @pedal.id, price: @pedal.price, quantity: 4)
+      @item_order_8 = @order_3.item_orders.create!(item_id: @reflector.id, price: @reflector.price, quantity: 7)
+      @item_order_9 = @order_3.item_orders.create!(item_id: @paper.id, price: @paper.price, quantity: 7)
+
+      merchant_item_orders = @order_3.merchant_item_orders(@mike)
+
+      expect(merchant_item_orders[0]).to eq(@item_order_9)
     end
   end
 end
