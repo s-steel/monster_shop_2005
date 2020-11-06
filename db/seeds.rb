cc71eef56692e861895e5ef05d9eb9e1a544997d
Aidan Murray <aidanhansm@gmail.com>
2020-11-05 17:46:34 -0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,14 @@ tire = bike_shop.items.create(name: 'Gatorskins', description: "They'll never po
 pull_toy = dog_shop.items.create(name: 'Pull Toy', description: 'Great pull toy!', price: 10, image: 'http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg', inventory: 32)
 dog_bone = dog_shop.items.create(name: 'Dog Bone', description: "They'll love it!", price: 21, image: 'https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg', active?: false, inventory: 21)
 
+admin_user = User.create(name: 'Default Admin',
+                          address: 'N/A',
+                          city: 'N/A',
+                          state: 'N/A',
+                          zip: 'N/A',
+                          email: 'admin@email.com',
+                          password: 'admin',
+                          role: 2)
 
 # @mike = Merchant.create(name: "Mikemart", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
 # @meg = Merchant.create(name: "Megmart", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
